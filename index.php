a98978f8d8518e5b1341280c5912765aaacef7dd
hapait <s.jamil1993@gmail.com>
2019-12-21 23:33:26 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>D DEV STORE</title>
+
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css"/>
+    <link rel="stylesheet" href="font-awsome\css\font-awesome.min.css"/>
+    <link rel="stylesheet" href="styles/style.css"/>
+
+</head>
+<body>
+    <h1>Test</h1>
+    <div id="top">
+        <div class="container"><!--Container Begin-->
+            <div class="col-md-6 offer"><!--col-md-6 offer Begin-->
+                <a href="#" class="btn btn-success btn-sm">Welcome</a>
+                <a href="checkout.php">4 Items In Cart | Total Price Is 300$</a>
+            </div><!--col-md-6 offer End-->
+            <div class="col-md-6"><!--col-md-6 Begin-->
+                <ul class="menu"><!--menu Begin-->
+                    <li>
+                        <a href="customer_register.php">Register</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">My Account</a>
+                    </li>
+                    <li>
+                        <a href="cart.php">Go to cart</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">Login</a>
+                    </li>
+                </ul><!--menu End-->
+            </div><!--col-md-6 End-->
+        </div><!--Container End-->
+    </div>
+    <h1>End</h1>
+    <script src="js\jquery-331.min.js"></script>
+    <script src="js\bootstrap-337.min.js"></script>
+</body>
+</html>
