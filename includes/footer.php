0ab019beb51af74e1547b7355f1f8044b92faf6f
hapait <s.jamil1993@gmail.com>
2019-12-23 16:16:11 +0600

    footer complete

---
+++ includes/footer.php
@@ -0,0 +1,93 @@
+<div id="footer"><!--#footer Begin-->
+    <div class="container"><!--container Begin-->
+        <div class="row"><!--row Begin-->
+            <div class="col-sm-6 col-md-3"><!--col-sm-6 col-md-3 Begin-->
+                
+                <h4>Pages</h4>
+
+                <ul><!--ul begin-->
+                    <li><a href="cart.php">Shopping Cart</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="checkout.php">My account</a></li>
+                </ul><!--ul end-->
+
+                <hr>
+                <h4>User Section</h4>
+                <ul><!--ul begin-->
+                    <li><a href="checkout.php">Login</a></li>
+                    <li><a href="customer_register.php">Register</a></li>
+                </ul><!--ul end-->
+
+                <hr class="hidden-md hidden-lg hidden-sm">
+            </div><!--col-sm-6 col-md-3 End-->
+
+            <div class="col-sm-6 col-md-3"><!--col-sm-6 col-md-3 Begin-->
+                
+                <h4>Top Products Categories</h4>
+
+                <ul><!--ul begin-->
+                    <li><a href="#">Jackets</a></li>
+                    <li><a href="#">Accessories</a></li>
+                    <li><a href="#">Coats</a></li>
+                    <li><a href="#">Shoes</a></li>
+                    <li><a href="#">T-Shirts</a></li>
+                </ul><!--ul end-->
+
+                <hr class="hidden-md hidden-lg">
+
+            </div><!--col-sm-6 col-md-3 End-->
+
+            <div class="col-sm-6 col-md-3"><!--col-sm-6 col-md-3 Begin-->
+
+                 <h4>Find us:</h4>
+                 <p><!--p begin-->
+                    <strong>Hapait Inc.</strong>
+                    <br/>500/3, Dhaka.
+                    <br/>+0881234555
+                    <br/>s.jamil1993@gmail.com
+                    <br/><strong>Shafayet</strong>
+                 </p><!--p end-->
+                 <a href="contact.php">Check Our Contact Page</a>
+                 <hr class="hidden-md hidden-lg">            
+            </div><!--col-sm-6 col-md-3 End-->
+
+            <div class="col-sm-6 col-md-3"><!--col-sm-6 col-md-3 Begin-->
+                <h4>Get The News</h4>
+                <p class="text-muted">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+                <form action="" method="post"><!--form begin-->
+                    <div class="input-group"><!--input-group begin-->
+                        <input type="text" class="form-control" name="email">
+                        <span class="input-group-btn"><!--input-group-btn begin-->
+                            <input type="submit" value="Subscribe" class="btn btn-default">
+                        </span><!--input-group-btn end-->
+                    </div><!--input-group end-->
+                </form><!--form end-->
+                <hr>
+                <h4>Keep in touch</h4>
+                <p class="social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-google-plus"></a>
+                    <a href="#" class="fa fa-envelope"></a>
+                </p>
+            </div><!--col-sm-6 col-md-3 End-->
+
+        </div><!--row End-->
+    </div><!--container End-->
+</div><!--#footer End-->
+
+<div id="copyright"><!--#copyright Begin-->
+    <div class="container"><!--container Begin-->
+        <div class="col-md-6"><!--col-md-6 Begin-->
+            <p class="pull-left">&copy; Shafayet Hapait</p>
+        </div><!--col-md-6 End-->
+
+        <div class="col-md-6"><!--col-md-6 Begin-->
+            <p class="pull-right">Theme By: <a href="#">Shafayet</a></p>
+        </div><!--col-md-6 End-->
+    </div><!--container End-->
+</div><!--#copyright End-->
--- index.php
+++ index.php
@@ -361,7 +361,11 @@
         </div><!--row end-->
     </div><!--container end-->
 
-    <script src="js\jquery-331.min.js"></script>
-    <script src="js\bootstrap-337.min.js"></script>
+    <?php 
+        include("includes/footer.php");
+    ?>
+
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
 </body>
 </html>
